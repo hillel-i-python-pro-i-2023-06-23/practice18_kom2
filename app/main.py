c0ff0c1402928e99a1c4eb22f90f1beb9e7628d5
nick <nikoladarkangel@gmail.com>
2023-08-27 22:20:25 +0300

    fix generation 4

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 import concurrent.futures
-
 from app.services.calk_max_variants import calk
 from app.services.generate_world import generate_word_regularly
 from app.services.save import save_in_file
 from app.services.use_simbol import use_simbols
 import time
 
-
-
 def main():
+
     print("Пароль состоит из _ елементов?")
     word_len = int(input())
 
@@ -17,12 +15,11 @@ def main():
 
     # rez = generate_word_recursive(use_simbols, word_len, )
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
         start_time_for_regularly = time.time()
         rez_for_regulary = generate_word_regularly(word_len, use_simbols, word_variants)
         print(len(rez_for_regulary))
         end_time_for_regularly = time.time()
         elapsed_time_for_regularly = end_time_for_regularly - start_time_for_regularly
 
-    save_in_file(rez_for_regulary, elapsed_time_for_regularly)
+        save_in_file(rez_for_regulary, elapsed_time_for_regularly)
