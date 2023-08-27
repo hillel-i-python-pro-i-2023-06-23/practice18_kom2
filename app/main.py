b6bd089bcc3d8001ca8b81ab61028fae969bf521
nick <nikoladarkangel@gmail.com>
2023-08-27 20:11:40 +0300

    fix generation 3

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
+import concurrent.futures
+
 from app.services.calk_max_variants import calk
 from app.services.generate_world import generate_word_regularly
 from app.services.save import save_in_file
 from app.services.use_simbol import use_simbols
 import time
 
-# import threading
 
 
 def main():
@@ -16,10 +17,12 @@ def main():
 
     # rez = generate_word_recursive(use_simbols, word_len, )
 
-    start_time_for_regularly = time.time()
-    rez1 = generate_word_regularly(word_len, use_simbols, word_variants)
-    print(len(rez1))
-    end_time_for_regularly = time.time()
-    elapsed_time_for_regularly = end_time_for_regularly - start_time_for_regularly
+    with concurrent.futures.ThreadPoolExecutor() as executor:
 
-    save_in_file(rez1, elapsed_time_for_regularly)
+        start_time_for_regularly = time.time()
+        rez_for_regulary = generate_word_regularly(word_len, use_simbols, word_variants)
+        print(len(rez_for_regulary))
+        end_time_for_regularly = time.time()
+        elapsed_time_for_regularly = end_time_for_regularly - start_time_for_regularly
+
+    save_in_file(rez_for_regulary, elapsed_time_for_regularly)
